4d1f4e9da12adf33de2e244a5c94851253636fad
Mani kannan.G <manikannan6380.gmail.com>
2024-08-09 09:33:43 +0530

    After finish full project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import Task1 from './task1';
+import Home from './component/home';
+import Navbar from './component/navbar';
+import SignInSide from './component/login';
+import Footer from './component/footer';
+import { Toaster } from 'sonner';
+import Privacy from './component/privacy';
+import Corporate from './component/about';
+import Farmer from './component/farmer';
+import FarmerHome from './component/farmerhome';
+import Marquee from './class_activity/Marque';
+import PaymentForm from './component/payment';
+import ProfilePage from './component/profile';
+import ProfileDropdown from './component/profiledropdown';
+import WorldMapWithCurrentLocation from './component/map';
+
+
+
+// import HigherOrderFunction from './component/higherorderfunction';
+// import Increment from './component/auto_inc_dec';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    {/* <Task1 /> */}
+    {/* <HigherOrderFunction /> */}
+    {/* <Increment /> */}
+    {/* <Home /> */}
+
+    <Toaster richColors />
+
+    <Navbar />
+    <br />
+    <br />
+ {/* <SignInSide /> */}
+ <Footer />
+ {/* <Marquee /> */}
+{/* <Farmer /> */}
+ {/* <Privacy /> */}
+ {/* <Corporate /> */}
+ {/* <FarmerHome /> */}
+    {/* <PaymentForm /> */}
+    {/* <ProfilePage /> */}
+    {/* <ProfileDropdown /> */}
+   {/* <WorldMapWithCurrentLocation /> */}
+   </div>
   );
 }
 
